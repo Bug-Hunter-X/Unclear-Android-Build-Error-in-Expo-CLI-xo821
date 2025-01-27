87e54c61600a2ed4f0e5f476e73fd24582ecc8c2
BugHunter-X <askzenai@gmail.com>
2025-01-27 10:00:40 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo CLI to build an Android app.  The error message is cryptic, often mentioning issues with Android build tools or Gradle.  The actual cause can vary.  One common scenario involves mismatched versions of Android SDK components or incorrect configurations within the `android` folder of your project.
